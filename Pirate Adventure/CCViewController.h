0d6e7519ab76ff4b8fafb950aee9adc08c0dede5
Eliot Arntz <arntzel@gmail.com>
2013-10-02 10:54:59 +0200

    Initial Commit

---
+++ Pirate Adventure/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Pirate Adventure
+//
+//  Created by Eliot Arntz on 10/2/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
